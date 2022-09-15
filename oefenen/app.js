643eec705ce43733d13cf9fa4f9330a4bb0490f2
Rouhi1432 <9016726@student.zadkine.nl>
2022-09-15 15:00:06 +0200

    opdracht3.2

--- oefenen/app.js
+++ oefenen/app.js
@@ -4,3 +4,21 @@ let myAge = 18;
 const diploma = true;
 let passed;
 document.write(myName)
+
+let a = 5 * 2 ;
+document.write (a)
+
+let num1 = 7 ;
+let num2 = 77;
+let answer = num1+num2;
+console.log (answer)
+
+
+let answer1 = num1-num2;
+console.log (answer1)
+
+let answer2 = num1*num2;
+console.log (answer2)
+
+let answer3 = num1/num2;
+console.log (answer3)
