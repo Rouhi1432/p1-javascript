572e073a5601b0bf3e4826a5641a2ac816e54b33
Rouhi1432 <9016726@student.zadkine.nl>
2022-10-06 15:04:09 +0200

    opdracht6.4

--- opdracht6.1/app.js
+++ opdracht6.1/app.js
@@ -1,4 +1,5 @@
 function inhoud_aangepast() {
     document.getElementById("titel").style.color = "red";
 }
+document.getElementById("titel").innerText = "bijna klaar met javascript";
 inhoud_aangepast();
---
+++ opdracht6.4/app.js
@@ -0,0 +1,16 @@
+function calculate(num, num2){
+  let answer = num + num2;
+  console.log(answer)
+}
+
+calculate(2345, 2)
+
+
+
+
+
+function multyplyByeFive(num){
+let answer = num* 5
+console.log(answer);
+}
+multyplyByeFive(800)
