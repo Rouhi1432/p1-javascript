b82fe826b26c6d1f19ad35e825382c69b6f24de2
Rouhi1432 <9016726@student.zadkine.nl>
2022-10-11 12:14:46 +0200

    opdrachat7.2

---
+++ opdracht7.2/app.js
@@ -0,0 +1,8 @@
+let product = prompt("Hoeveel kost het product dat je wilt kopen?");
+let budget = 100;
+
+if(budget > product) {
+    console.log("mf ik heb genoeg geld");
+} else {
+    console.log("ik heb niet genoeg geld");
+}
